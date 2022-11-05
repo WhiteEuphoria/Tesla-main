1aefc77292ea20a1192a6b5bfba1c499d8de708b
WhiteEuphoria <prognikitos@gmail.com>
2022-11-05 04:56:12 +0200

    Tesla-main

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function($) {
+    $.widget('custom.phonecode', {
+        options: {
+            default_prefix: '',
+            prefix: '',
+            preferCo: 'ru'
+        },
+        _create: function() {
+
+        },
+    });
+})(jQuery);
