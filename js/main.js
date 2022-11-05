04acdbe2ef8c0e906417b92aa24e37b05fddc1f9
WhiteEuphoria <prognikitos@gmail.com>
2022-11-05 07:57:02 +0200

    burger

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
-(function($) {
-    $.widget('custom.phonecode', {
-        options: {
-            default_prefix: '',
-            prefix: '',
-            preferCo: 'ru'
-        },
-        _create: function() {
+// (function($) {
+//     $.widget('custom.phonecode', {
+//         options: {
+//             default_prefix: '',
+//             prefix: '',
+//             preferCo: 'ru'
+//         },
+//         _create: function() {
 
-        },
-    });
-})(jQuery);
+//         },
+//     });
+// })(jQuery);
+const burgerBtn = document.querySelector(".burger");
+const burgerClose = document.querySelector(".nav-menu__wrapp");
+
+
+burgerBtn.addEventListener("click", function (){   
+
+    document.querySelector(".burger").classList.toggle("burger--active");
+
+    document.querySelector(".nav-menu").classList.toggle("nav-menu--active");
+
+ 
+});
+
+burgerClose.addEventListener("click", function (){   
+
+    document.querySelector(".burger").classList.toggle("burger--active");
+
+    document.querySelector(".nav-menu").classList.toggle("nav-menu--active");
+
+});
